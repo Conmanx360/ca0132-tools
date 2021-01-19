7ef660dc83ef72b449a3bf5d264aab9835c4a895
Connor McAdams <conmanx360@gmail.com>
2021-01-19 16:06:03 -0500

    ca0132-tools: Update ca0132-dsp-assembler.
    Update ca0132-dsp-assembler program, to allow the user to set the name
    of the output binary file.
    
    Signed-off-by: Connor McAdams <conmanx360@gmail.com>

--- ca0132-dsp-assembler.c
+++ ca0132-dsp-assembler.c
@@ -31,44 +31,51 @@ static uint32_t get_next_asm_op_str(FILE *asm_file, FILE *tmp, char *buf)
 	return ret;
 }
 
-int main(int argc, char **argv)
+static int read_assembly_from_file(char *asm_file_name, char *data_file_name)
+{
+	FILE *asm_file, *data_file;
+	dsp_asm_data data;
+	char buf[0x200];
+
+	asm_file = fopen(asm_file_name, "r");
+	if (!asm_file) {
+		printf("Failed to open asm file!\n");
+		return 1;
+	}
+
+	data_file = fopen(data_file_name, "w+");
+	if (!data_file) {
+		fclose(asm_file);
+		printf("Failed to open data file!\n");
+		return 1;
+	}
+
+	memset(&data, 0, sizeof(data));
+	memset(buf, 0, sizeof(buf));
+	while (get_next_asm_op_str(asm_file, data_file, buf)) {
+		get_asm_data_from_str(&data, buf);
+		fwrite(data.opcode, sizeof(data.opcode[0]),
+				get_dsp_op_len(data.opcode[0] >> 16), data_file);
+
+		memset(&data, 0, sizeof(data));
+		memset(buf, 0, sizeof(buf));
+	}
+
+	fclose(data_file);
+	fclose(asm_file);
+
+	return 0;
+}
+
+static int read_assembly_from_terminal()
 {
 	dsp_asm_data data;
 	char buf[0x200];
 	uint32_t i = 0;
-	FILE *asm_file, *data_file;
-
-	if (argc < 1) {
-		printf("Usage: %s <file>\n", argv[0]);
-		return -1;
-	}
-
 
 	memset(&data, 0, sizeof(data));
-	if (argc > 1) {
-		asm_file = fopen(argv[1], "r");
-		data_file = fopen("test-file.bin", "w+");
-		if (!asm_file) {
-			printf("Failed to open asm file!\n");
-			return 1;
-		}
+	memset(buf, 0, sizeof(buf));
 
-		while (get_next_asm_op_str(asm_file, data_file, buf)) {
-			get_asm_data_from_str(&data, buf);
-			fwrite(data.opcode, sizeof(data.opcode[0]),
-					get_dsp_op_len(data.opcode[0] >> 16), data_file);
-
-			memset(&data, 0, sizeof(data));
-			memset(buf, 0, sizeof(buf));
-		}
-
-		fclose(data_file);
-		return 0;
-	}
-
-/*
-	print_op_layouts();
-*/
 	while (scanf("%c", &buf[i])) {
 		if (buf[i] == ';') {
 			buf[i + 1] = '\0';
@@ -83,7 +90,29 @@ int main(int argc, char **argv)
 	printf("Op %s: ", data.op.op_str);
 	for (i = 0; i < 4; i++)
 		printf("0x%08x ", data.opcode[i]);
+
 	putchar('\n');
 
 	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	int ret;
+
+	ret = 0;
+	if ((argc > 1) && (argc < 3)) {
+		printf("Usage: %s <asm_file> <output_file>\n", argv[0]);
+		printf("Or, no arguments, and enter the assembly string into the terminal.\n");
+		goto exit;
+	}
+
+	if (argc > 2)
+		ret = read_assembly_from_file(argv[1], argv[2]);
+	else
+		ret = read_assembly_from_terminal();
+
+exit:
+
+	return ret;
 }
